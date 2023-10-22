62b2492ccbee5aa255d1bcb934beb4c9d1c8bcdf
bciereszynski <b.ciereszynski@wp.pl>
2023-10-22 19:42:38 +0200

    LA-3 remove style sheet

--- src/main.py
+++ src/main.py
@@ -5,11 +5,6 @@ from PyQt5.QtWidgets import QApplication
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyle('Breeze')
-    app.setStyleSheet('''
-        QWidget {
-            font-size: 35px;
-        }
-    ''')
 
     window = MainWindow()
     window.show()
